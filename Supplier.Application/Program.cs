420e3b2639b1e1d94746af8290a06044466d2e43
henrique <henriquehvisa@gmail.com>
2020-05-09 11:19:37 -0300

    Initial Commit

---
+++ Supplier.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supplier.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
